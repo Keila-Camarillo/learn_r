7528fc1711d0b4f13997c75b45468091ba7e8a16
Keila-Camarillo <keila.a.camarillo@gmail.com>
2023-08-01 10:00:56 -0500

    add r basics test

---
+++ R_for_programmers.R
@@ -0,0 +1,18 @@
+
+# an object using the assignment operator, an arrow sign (<-)
+minutes_in_year <- 525600
+print(minutes_in_year)
+
+
+# Currently, the object foot_to_centimeters is assigned the value 0. 
+# Change the assignment of foot_to_centimeters from 0 to an operation that computes the number of centimeters in a foot
+# (note: there are 2.54 centimeters in an inch and 12 inches in a foot).
+
+foot_to_centimeters <- 2.54*12
+print(foot_to_centimeters)
+
+### Name Rules
+# 1. No spaces (minutes in year does not work)
+# 2. Only . and _ are acceptable punctuation (minutes.in.year and minutes_in_year and minutesinyear will all work and are all different names).
+# 3. Names cannot begin with a punctuation mark.
+# 4. Names can contain numbers, but cannot start with a number (2times5 does not work, but prod_2_5 will work)
